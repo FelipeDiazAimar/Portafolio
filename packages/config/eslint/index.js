b7e36751c29367ee927abce552c768ec74033737
FelipeDiazAimar <felipediazaimar@gmail.com>
2026-07-18 13:05:37 -0300

    feat(config): add shared design tokens and eslint config

---
+++ packages/config/eslint/index.js
@@ -0,0 +1,17 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
